edd4188982629ca34cf2babbc40c8ad75afc7f4a
ebrahimi <Ebrahimi.Mahdi@Tara360.ir>
2024-03-14 10:33:32 +0330

    commit welcome

--- product/src/main/java/org/ebrahimi/ProductApplication.java
+++ product/src/main/java/org/ebrahimi/ProductApplication.java
@@ -13,5 +13,7 @@ public class ProductApplication
     public static void main( String[] args )
     {
         SpringApplication.run(ProductApplication.class, args);
+
+        System.out.println( "Hello World!" );
     }
 }
